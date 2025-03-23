420297532341926b1278e0f18c9f536bcd0dcb19
MeliAjna <alijiajn@students.zhaw.ch>
2025-03-23 20:53:03 +0100

    kp

--- Start.py
+++ Start.py
@@ -19,7 +19,7 @@ login_manager.login_register()  # open login/register page
 
 # load the data from the persistent storage into the session state
 data_manager.load_user_data(
-    session_state_key='data_df', 
+    session_state_key='calorie_data_df', 
     file_name='data.csv', 
     initial_value = pd.DataFrame(), 
     parse_dates = ['timestamp']
